b8ce0bd15eddf503f616322f707ffdae1ad3204b
lunacoyle <lunacoyle25@marlborough.org>
2024-09-16 14:58:36 -0700

    k

--- unit1/Fizzbuzz.java
+++ unit1/Fizzbuzz.java
@@ -1,8 +1,30 @@
 //
-//Fizzbuzz.java
+//FizzBuzz.java
 //
 
-public class Fizzbuzz
+public class FizzBuzz
 {
-     public static void main(String[] args)
+    public static void main(String[] args)
+    {
+        System.out.println("FizzBuzz");
+
+        for (int i=1; i<31; i++)
+        {
+            if (i%15 == 0)
+            {
+                System.out.println("FizzBuzz");
+            } 
+            else if (i%3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if (i%5 == 0)
+            { 
+                  System.out.println("Buzz");  
+            }
+            {
+                  System.out.println(i);  
+            }
+        }
+    }
 }
