5c00783110973559e50ece062abc05ee29faa2cc
lunacoyle <lunacoyle25@marlborough.org>
2024-09-16 00:06:20 -0700

    hi

---
+++ unit1/Multiplesof3.java
@@ -0,0 +1,31 @@
+//
+// Multiplesof3.java
+//
+
+
+public class Multiplesof3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Sequences");
+        System.out.println();
+
+        System.out.println();for (int i=0; i<30; i+=3)
+            System.out.println(i);
+
+    
+        System.out.println();
+
+        int value = 0;
+
+        for (int i=0; i<10; i++)
+        {
+            System.out.println(value);
+            value += 3;
+        }
+
+        System.out.println();
+
+
+    }
+}
