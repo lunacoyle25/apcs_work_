3d50c53d6faaad7c10dc4502df35b4b0f09b26d6
lunacoyle <lunacoyle25@marlborough.org>
2024-09-19 16:12:10 -0700

    j

---
+++ unit1/Cubes.java
@@ -0,0 +1,17 @@
+//
+//Cubes.java
+//
+
+public class Cubes 
+{
+    public static void main (String[] args)
+    {
+        int intialTerm = 1;
+        int numberOfTerms = 15;
+        
+        for (int i = intialTerm; i < numberOfTerms; i++)
+        {
+            System.out.println(i*i*i);
+        }
+    }
+}
