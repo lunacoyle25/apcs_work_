3ace096f6e3fec275f5bd03b235ddc4def5a27a7
lunacoyle <lunacoyle25@marlborough.org>
2024-09-19 16:04:07 -0700

    l

---
+++ unit1/Geometric.java
@@ -0,0 +1,25 @@
+//
+//Geometric.java
+//
+
+public class Geometric
+{
+    
+    public static void main(String[] args)
+    {
+        int initialTerm =3; //First term
+        int commonRatio = 2; // Common ratio
+       int numberOfTerms = 10; // Number of terms to print
+        
+        int term = initialTerm; // Start with intial term
+        
+        // Print the first terms of the geometric sequence
+        for (int i = 0; i < numberOfTerms; i++){
+             // Why do I have to captialize the second word but no space? Is that camel or smth?
+        
+            System.out.println(term);
+            term*= commonRatio; //Update term by multiplying with the common ratio
+        }
+            
+    }
+ }
