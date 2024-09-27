958dd08d8191b66c085d11f49291aef8edb972a9
lunacoyle <lunacoyle25@marlborough.org>
2024-09-27 15:13:17 -0700

    k

---
+++ IcyHot.java
@@ -0,0 +1,36 @@
+//
+//IcyHot.java
+//
+
+public class IcyHot{
+    public static boolean IcyHot(int temp1, int temp2)
+    {
+        if ((temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 >100))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    } 
+    
+    public static void testIcyHot(int temp1, int temp2, boolean expected)
+    {
+      boolean result = IcyHot(temp1, temp2); 
+        System.out.println(" temp 1: " + temp1 + " temp 2: " + temp2 + " expected: " + expected + " result: " + result);
+        if (result == expected)
+        {
+            System.out.println("correct");
+        }
+        else
+        {
+            System.out.println("incorrect");
+        }
+    }
+        
+    public static void main(String[] args)
+    {
+        testIcyHot(-1, 200, true);
+    }
+}
