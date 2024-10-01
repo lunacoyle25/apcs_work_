3816419fd8b3a41a599acc25b9a8dd111523a896
lunacoyle <lunacoyle25@marlborough.org>
2024-10-01 13:32:25 -0700

    l

---
+++ unit2/goodDeal.java
@@ -0,0 +1,38 @@
+/* 
+Unit 2 Assignment 3
+Luna Coyle 10/1/24
+*/
+
+public class goodDeal 
+{
+    public static void main(String[] args)
+    {
+        testGoodDeal(6,4,true);
+        testGoodDeal(20,14,true);
+        testGoodDeal(20,17,false);
+    }
+    public static boolean goodDeal(double originalPrice, double salePrice)
+    {
+      if (salePrice < .75*originalPrice)
+          {
+              return true;
+          }
+          else
+          {
+              return false; 
+          }
+    }
+    public static void testGoodDeal(double originalPrice, double salePrice, boolean expected)
+    {
+        boolean result = goodDeal(originalPrice, salePrice);
+        System.out.println("Original Price: " + originalPrice + " Sale Price: " + salePrice + " Expected: " + expected + " Result: " + result);
+        if (result == expected)
+        {
+            System.out.println("True"); 
+        }
+        else 
+        {
+            System.out.println("False");
+        }
+    }
+}
