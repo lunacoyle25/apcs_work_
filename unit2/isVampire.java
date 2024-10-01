b58b36a9b3cba17a3bf5a7d897772c1e6823a78e
lunacoyle <lunacoyle25@marlborough.org>
2024-10-01 13:02:39 -0700

    t

--- unit2/isVampire.java
+++ unit2/isVampire.java
@@ -7,7 +7,7 @@ public class isVampire
 {
     public static boolean isVampire(float hour, boolean awake)
     {
-        if(((hour < 6.0 && hour > 22.0) && (awake == true)) || !awake && (hour < 22 && hour > 6))
+        if((hour < 6.0 || hour > 22.0) && awake || !awake == true && (hour < 22 && hour > 6))
            {
                return true;
            }
@@ -16,23 +16,23 @@ public class isVampire
             return false; 
         }
     }
-    public static String testIsVampire(float hour, boolean awake, boolean expected)
+    public static void testIsVampire(float hour, boolean awake, boolean expected)
     {
         boolean result = isVampire(hour, awake); 
         System.out.println("hour: " + hour + " awake: " + awake +" expected: " + expected + " result: " + result);
         if (result == expected)
         {
-            return("True"); 
+            System.out.println("True"); 
         }
         else 
         {
-            return("False");
+            System.out.println("False");
         }
     }
     public static void main(String[] args)
     {
-        testIsVampire(3,true,true);
-        testIsVampire(7,false,false);
+        testIsVampire(3,false,false);
+        testIsVampire(7,true,false);
         testIsVampire(23,false,false);
         testIsVampire(23,true,true);
     }
