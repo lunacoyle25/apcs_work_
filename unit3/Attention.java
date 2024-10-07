37b933c7296bcd1cb0aa1a037d9eeff3ac641268
lunacoyle <lunacoyle25@marlborough.org>
2024-10-07 12:14:41 -0700

    l

---
+++ unit3/Attention.java
@@ -0,0 +1,43 @@
+/* 
+Assignment 3
+Luna Coyle 
+10/7/24
+*/ 
+
+public class Attention
+{
+    public static boolean attention(String input)
+    {
+        String str = "Hey you!";
+        if (input.length() >= 8 && input.substring(0,8).equals(str))
+      {
+            return true;
+      }
+        else
+        {
+            return false;
+        }
+    }
+    public static void testAttention(String input, boolean expected)
+    {
+        boolean result = attention(input);
+        System.out.println("input: " + input + "  expected: " + expected + "  result: " + result);
+        
+        if (result == expected)
+        {
+            System.out.println("correct"); 
+        }
+        else 
+        {
+            System.out.println("incorrect");
+        }
+    }
+    public static void main(String[] args)
+    {
+        testAttention("Hey", false);
+        testAttention("Sup, pretty lady", false);
+        testAttention("My name is Jane", false);
+        testAttention("Hey you! Give me your money", true);
+        testAttention("Hey you!", true);
+    }
+}
