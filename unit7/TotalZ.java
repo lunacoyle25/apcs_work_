3835a21beeedd0165b889e036c5e95d3345ce2bd
lunacoyle <lunacoyle25@marlborough.org>
2024-12-01 23:50:19 -0800

    d

--- unit7/TotalZ.java
+++ unit7/TotalZ.java
@@ -8,22 +8,45 @@ public class TotalZ
 {
     public static int totalZ(String[] dune)
     {
+        int total = 0;
         
+        for (int i = 0; i<dune.length; i++)
+        {
+            if ((dune[i].substring(0,1).equals("Z")) || (dune[i].substring(0,1).equals("z")))
+            {
+                total += dune[i].length();
+            } 
+        }
+        return total;
     }
     public static void testTotalZ(String[] dune, int expected)
     {
-        
+        int result = totalZ(dune);
+        for (String value : dune)
+        {
+            System.out.println(value + " ");
+        }
+        System.out.println("expected: " + expected + " result: " + result);
+        if (result == expected)
+        {
+            System.out.println("true");
+        }
+        else 
+        {
+            System.out.println("false");
+        }
+        System.out.println();
     }
     public static void main(String[] args)
     {
-        String [] dune = {Dr. Kessner, Tessa};
-        String [] dune1 = {Zelda, Mario, Luigi, Toshi};
-        String [] dune2 = {zitch, zoomies, Zonkey};
-        String [] dune3 = {};
+        String[] dune = {"Dr. Kessner", "Tessa"};
+        String[] dune1 = {"Zelda", "Mario", "zuigi", "Yoshi"};
+        String[] dune2 = {"ziltch", "zoomies", "Zonkey"};
+        String[] dune3 = {"zoologist", "zebra", "zookeeper", "pink panda"};
         
         testTotalZ(dune, 0);
-        testTotalZ(dune1, 1);
-        testTotalZ(dune2, 3);
-        testTotalZ(dune3, 0);
+        testTotalZ(dune1, 10);
+        testTotalZ(dune2, 19);
+        testTotalZ(dune3, 23);
     }
 }
