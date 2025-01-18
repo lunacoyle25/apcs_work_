d13bb06056585d19dca3790bd2a37638d6a7acd0
lunacoyle <lunacoyle25@marlborough.org>
2025-01-17 18:28:06 -0800

    s

---
+++ Project/Bounce.java
@@ -0,0 +1,37 @@
+/* Luna Coyle 1/14/25
+Starter Processing Display 
+*/ 
+
+import processing.core.*; 
+
+public class Bounce extends PApplet
+{ 
+    public void settings()
+    {
+        size(500, 500);
+    }
+    public void setup()
+    {
+        circle = new ArrayList<Circle>();
+        circle.add(new Circle(this));
+    }
+    public void draw()
+    {
+        background(B27FEE);
+        for (Circle x : cirlces)
+        {
+            x.display(); 
+        }
+    }
+    public void keyPressed()
+    {
+        for (int i=0; i<10; i++)
+        balls.add(new Ball(this)); 
+    }
+    private ArrayList<Circle> circles;
+    
+    public static void main(String[] args)
+    {
+        PApplet.main("Bounce");
+    }
+}
