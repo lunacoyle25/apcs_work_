00891c1caae3eec23c6232cd994f7938518982d4
lunacoyle <lunacoyle25@marlborough.org>
2025-02-25 16:56:14 -0800

    l

--- Project/ScenePlay.java
+++ Project/ScenePlay.java
@@ -28,11 +28,11 @@ public class ScenePlay implements Scene {
         //p.background(197, 234, 250); 
         
         float x1 = centerX;
-        float y1 = p.height; // Bottom tip touching the ground
+        float y1 = p.height; //bottom tip touching the ground
         float x2 = centerX - triangleWidth / 2;
-        float y2 = p.height - triangleHeight; // Top-left corner
+        float y2 = p.height - triangleHeight; // top-left corner
         float x3 = centerX + triangleWidth / 2;
-        float y3 = p.height - triangleHeight; // Top-right corner
+        float y3 = p.height - triangleHeight; // top-right corner
 
         p.fill(196, 164, 132);
         p.triangle(x1, y1, x2, y2, x3, y3);
@@ -44,7 +44,7 @@ public class ScenePlay implements Scene {
     }
     
     private PApplet p;
-    private float centerX; // X position (center of the triangle)
+    private float centerX; // x position (center of the triangle)
     private float speed; 
     private float triangleWidth; 
     private float triangleHeight; 
--- Project/SceneStart.java
+++ Project/SceneStart.java
@@ -30,10 +30,79 @@ public class SceneStart implements Scene
    public void display()
    {
         p.background(255, 209, 223);
-        p.textSize(90);
+        
         p.textAlign(p.CENTER, p.CENTER);
-        p.text("Scoop Madness", p.width/2, p.height/2);  
-        p.image(icecreamGif, 200, 350, p.width/2, p.height/2);
+        p.textSize(90);
+        String s = "Scoop";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (i%2 == 0) //even
+            {
+                p.fill(86, 190, 179); //teal 
+                p.text(s.substring(i, i+1), (p.width-570)+43*i, p.height/4);
+            }
+            else 
+            {
+                p.fill(255, 255, 255); //white
+                p.text(s.substring(i, i+1), (p.width-570)+43*i, p.height/4);
+            }
+        }
+        
+        p.fill(86, 190, 179);
+        p.text("M", p.width-315, p.height/4); 
+        
+        p.textSize(95);
+        String a = "adne"; 
+        for (int i = 0; i < a.length(); i++)
+        {
+            if (i%2 == 1) 
+            {
+                p.fill(86, 190, 179); //teal 
+                p.text(a.substring(i, i+1), (p.width-245)+43*i, p.height/4);
+            }
+            else 
+            {
+                p.fill(255, 255, 255); //white
+                p.text(a.substring(i, i+1), (p.width-245)+43*i, p.height/4);
+            }
+        }
+        p.textSize(97);
+        String u = "ss"; 
+        //u for the missing u between s&s that would make sus
+        for (int i = 0; i < u.length(); i++)
+        {
+            if (i%2 == 1) 
+            {
+                p.fill(86, 190, 179); //teal 
+                p.text(u.substring(i, i+1), (p.width-84)+35*i, p.height/4);
+            }
+            else 
+            {
+                p.fill(255, 255, 255); //white
+                p.text(u.substring(i, i+1), (p.width-84)+35*i, p.height/4);
+            }
+        }
+        
+        /*p.fill(255, 255, 255);
+        //p.fill(199, 25, 0);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.textSize(90);
+        p.text(s, p.width/2, p.height/4); */
+        
+        
+        p.fill(255, 255, 255);
+        //p.fill(199, 25, 0);
+        p.textSize(40);
+        p.text("Try to stack the scoops", p.width-415, 13*p.height/32);
+        p.text("as high as you can!", p.width-430, 15*p.height/32);
+        
+        p.fill(139, 62, 49);
+        //p.fill(255, 255, 255);
+        p.textSize(24);
+        p.text("Press any key to play", p.width/4, 19*p.height/32);
+        
+        p.image(icecreamGif, 300, 300, p.width/2, p.height/2);
+        
    }
    
    private PApplet p;
